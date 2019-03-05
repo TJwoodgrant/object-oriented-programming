cb5caec3f2b7738fe03faa536cbe234c36895ba8
Mikan <mikanwolfe@nekox.net>
2019-03-05 12:53:22 +1100

    Pt1.2, Pt1.3

---
+++ pt1.2-c-sharp-reference.md
@@ -0,0 +1,2 @@
+# C# Reference Sheet
+
---
+++ pt1.3/pt1.3/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pt1._3
+{
+    class MainClass
+    {
+
+        static void PrintCounters(Counter[] counters)
+        {
+            Counter c;
+
+            foreach (Counter c in counters)
+            {
+                Console.WriteLine("{0} is {1}", c.Name,c.Value);
+            }
+        }
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
