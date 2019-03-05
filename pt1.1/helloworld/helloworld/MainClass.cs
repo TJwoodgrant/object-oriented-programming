cc17097bb9c8bf6f74c576fa75d7ace3e97e875a
Mikan <mikanwolfe@nekox.net>
2019-03-05 11:12:53 +1100

    initial commit + pt1-1

---
+++ pt1.1/helloworld/helloworld/MainClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace helloworld
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Message myMessage;
+            Message[] messages = new Message[5];
+            string name;
+
+            myMessage = new Message("Hello World...");
+            myMessage.Print();
+
+           
+            messages[0] = new Message("if you're seeing this then the author forgot to impleme--");
+            messages[1] = new Message("probably will try for a credit again");
+            messages[2] = new Message("probably knows more about programming than you think");
+            messages[3] = new Message("Requires elevated permissions! Run program as sudo to continue!");
+
+            Console.WriteLine("Enter Name: ");
+            name = Console.ReadLine();
+
+            if (name.ToLower() == "jimmy")
+            {
+                messages[0].Print();
+            }
+            else if (name.ToLower() == "danny")
+            {
+                messages[1].Print();
+            }
+            else if (name.ToLower() == "klim")
+            {
+                messages[2].Print();
+            }
+            else
+            {
+                messages[3].Print();
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pt1.1/helloworld/helloworld/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helloworld
+{
+    public class Message
+    {
+        private string text;
+
+        public Message(string txt)
+        {
+            text = txt;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
