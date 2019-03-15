d0eff24ee1e84eeb584ed2e145d11ae1ac62e122
Mikan <mikanwolfe@nekox.net>
2019-03-15 16:22:12 +1100

    update

--- pt4.2/pt2.2/pt2.2/Inventory.cs
+++ pt4.2/pt2.2/pt2.2/Inventory.cs
@@ -16,6 +16,10 @@ namespace pt2._2
 
         public bool HasItem(string id)
         {
+
+            if (_items.Count == 0)
+                return false;
+
             foreach(Item i in _items)
             {
                 if (i.AreYou(id))
@@ -32,17 +36,41 @@ namespace pt2._2
 
         public Item Take(string id)
         {
-            throw new NotImplementedException();
+            foreach(Item i in _items)
+            {
+                if (i.AreYou(id))
+                {
+                    Item itemFound = i;
+                    _items.Remove(i);
+                    return itemFound;
+                }
+            }
+            return null;
         }
 
         public Item Fetch(String id)
         {
-            throw new NotImplementedException();
+            foreach(Item i in _items)
+            {
+                if (i.AreYou(id))
+                    return i;
+            }
+            return null;
         }
 
         public string ItemList
         {
-            get => null;
+            get
+            {
+
+                string list = string.Empty;
+                foreach(Item i in _items)
+                {
+                    list = list + "    " + i.ShortDescription + System.Environment.NewLine;
+                }
+                return list;
+            }
+
         }
 
         
@@ -51,13 +79,14 @@ namespace pt2._2
     [TestFixture]
     class TestInventory
     {
-        Inventory i = new Inventory();
         Item itm = new Item(new string[] { "potion" }, "red", "A bitter-smelling red potion");
+        Item itm2 = new Item(new string[] { "potion" }, "blue", "A sour-smelling blue potion");
 
         [Test]
         public void TestFindItem()
         {
-
+            Inventory i = new Inventory();
+            
             i.Put(itm);
             bool actual = i.HasItem(itm.FirstID);
 
@@ -67,7 +96,9 @@ namespace pt2._2
         [Test]
         public void TestNotFindItem()
         {
+            Inventory i = new Inventory();
 
+            i.Put(itm);
             bool actual = i.HasItem("pen");
 
             Assert.IsFalse(actual, "Test Inventory does not have item by FirstID");
@@ -76,7 +107,38 @@ namespace pt2._2
         [Test]
         public void TestFetchItem()
         {
+            Inventory i = new Inventory();
 
+            i.Put(itm);
+            Item takenItem = i.Fetch(itm.FirstID);
+
+            Assert.AreEqual(takenItem, itm, "Test Inventory Fetch such that item is not taken");
+        }
+
+        [Test]
+        public void TestTakeItem()
+        {
+            Inventory i = new Inventory();
+
+            i.Put(itm2);
+
+            Item takenItem = i.Take(itm2.FirstID);
+
+            bool actual = i.HasItem(itm2.FirstID);
+            Assert.IsFalse(actual, "Test Inventory Take such that Item cannot be found by AreYou");
+
+        }
+
+        [Test]
+        public void TestInventoryItemList()
+        {
+            Inventory i = new Inventory();
+
+            i.Put(itm);
+            i.Put(itm2);
+            string actual = i.ItemList;
+            string expected = "    a red potion\r\n    a blue potion\r\n";
+            Assert.AreEqual(expected, actual, "Test Inventory List String, should be a formatted list");
         }
 
 
