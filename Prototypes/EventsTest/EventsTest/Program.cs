5166bb6069c309a7fbbb4cadd8547c7685401829
Mikan <mikanwolfe@nekox.net>
2019-04-05 08:36:53 +1100

    create prototypes

---
+++ Prototypes/EventsTest/EventsTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EventsTest
+{
+    class Program
+    {
+
+        public delegate void delegatePrint(int Value);
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            /*
+             * Passing functions as parameters: Delegates essentially act as pointers towards 
+             *  functions. It's a reference data type that holds the reference of a method.
+             *  
+             * They can be declared using the *delegate* keyword.
+             * 
+             * Here's an example.
+             * 
+             */
+
+
+            delegatePrint printDel = PrintNumber;
+
+            printDel(1000);
+
+            printDel = PrintMoney;
+
+            printDel(1000);
+
+
+            Console.ReadLine();
+
+
+        }
+
+
+        public static void PrintNumber(int num)
+        {
+            Console.WriteLine("Number: {0, -12:N0}", num);
+        }
+
+        public static void PrintMoney(int num)
+        {
+            Console.WriteLine("Money: {0:C}", num);
+        }
+
+    }
+}
