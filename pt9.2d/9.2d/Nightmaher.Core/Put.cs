82b0b4d1528366b3e95e5616a999ab98ba205cb2
Mikan <ivory@nekox.net>
2019-05-15 12:23:35 +1000

    9.2D working on PUT command

---
+++ pt9.2d/9.2d/Nightmaher.Core/Put.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nightmaher.Core
+{
+    public class Put : Command
+    {
+
+        public Put()
+            : base(new string[] { "put" })
+        {
+            
+        }
+
+
+        public override string Execute(Player p, string[] text)
+        {
+            //Put NOUN on/in NOUN (Put Sweet on Table)
+            //put  * --> Error
+            throw new NotImplementedException("sorry.");
+        }
+    }
+}
