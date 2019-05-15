9aeef16f19567fd0007651c1c306de00aea363db
Mikan <ivory@nekox.net>
2019-05-15 12:23:26 +1000

    6.1C added unit tests

--- pt6.1c/6.1c/pt2.2/Location.cs
+++ pt6.1c/6.1c/pt2.2/Location.cs
@@ -38,7 +38,7 @@ namespace pt2._2
         Player p;
 
         [Test]
-        public void TestLocationIdentifyItself()
+        public void TestPlayerCanLocateLocation()
         {
             p = new Player("Tohru", "O No Not More References Y U Do Dis");
             l = new Location("Classroom", "A classroom with a few rows of long tables. Each table seems to contain five pieces of junk known as iMacs. ");
@@ -47,6 +47,38 @@ namespace pt2._2
             bool actual = l.AreYou("location");
 
             Assert.IsTrue(actual, "Test Location can identify itself as 'location'");
+        }
+
+        [Test]
+        public void TestPlayerHasLocation()
+        {
+            p = new Player("Miki Sano", "Admiral of the Kotona Choir");
+            l = new Location("Classroom", "A classroom with a long rows of tables. Each table seems to contain " +
+                "pieces of junk known as iMacs. ");
+
+            p.Location = l;
+
+            GameObject expected = l;
+            GameObject actual = p.Locate("room");
+
+            Assert.AreEqual(expected, actual, "Test if the player can locate their location using the Locate Command");
+
+           
+        }
+
+        [Test]
+        public void TestLocationIdentifyItems()
+        {
+            l = new Location("Classroom", "A classroom with a long rows of tables. Each table seems to contain " +
+                "pieces of junk known as iMacs. ");
+            Item i = new Item(new string[] { "potion" }, "red", "A bitter-smelling red potion");
+            l.Inventory.Put(i);
+
+            GameObject expected = i;
+            GameObject actual = l.Locate("potion");
+
+            Assert.AreEqual(expected, actual, "Test Location can identify itself as 'location'");
+
         }
         
 
